aee0d76a42907d177302f9d1d73171c21a13ce9a
William Wise <>
2017-12-01 08:35:07 +1100

    Added a catch for the profile user

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -25,6 +25,28 @@ export default {
           context.commit('setUser', false)
         }
       })
+      .catch((error) => {
+        console.error('Failed to get user', error.response)
+        if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          context.dispatch('notifications/push', {
+            title: 'Poor connection',
+            body: "We're having trouble connecting to the LiveHUB servers. Check your Internet connection.",
+            class: 'is-warning'
+          }, { root: true })
+        } else {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx, or
+          // something happened in setting up the request that triggered an Error
+          context.dispatch('notifications/push', {
+            title: 'Internal error',
+            body: "A problem occured when looking up this user. Please try again.",
+            class: 'is-warning'
+          }, { root: true })
+        }
+      })
     }
   }
 }
